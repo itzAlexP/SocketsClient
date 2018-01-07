8d4672f7baff4013beb12d79e7fbb15ce13671d9
itzAlexP <aperezbcn94@gmail.com>
2018-01-07 13:22:48 -0800

    Finalizado reggistro usuario

--- main.cpp
+++ main.cpp
@@ -9,15 +9,17 @@
 using namespace std;
 
 std::vector<string>
-vRazas,
 vPersonajes;
 
 sf::TcpSocket
 socket;
 
+sf::Socket::Status
+status;
+
 char
 cBufferSocket[100],
-cBufferSocketLong[2000];
+cBufferSocketLong2[2000];
 
 
 int
@@ -27,7 +29,6 @@ std::string
 sUserNick,
 sUserPass,
 sInfo,
-sRazas,
 sPersonajes;
 
 size_t
@@ -39,14 +40,124 @@ bVerifiedPassword = false,
 bVerifiedNewUser = false,
 bVerifiedRace = false,
 bVerifiedCharacterName = false,
-bSelectedCharacter = false;
+bSelectedCharacter = false,
+bSelectedListCharacter = false;
 
+
+void CrearPersonaje()
+{
+    char cBufferSocketLong[2000];
+    std::vector <string> vRazas;
+    string sRazas;
+
+    bVerifiedRace = false;
+    bVerifiedCharacterName = false;
+    iCounter = 0;
+
+    system("clear");
+
+    //Seleccionamos raza para el personaje
+    status = socket.receive(cBufferSocketLong, sizeof(cBufferSocketLong), received);
+
+    std::cout << "Selecciona raza\n" << std::endl;
+    sRazas.clear();
+    for(int i = 0; i < sizeof(cBufferSocketLong); i ++)
+    {
+
+        if(cBufferSocketLong[i] != '-')
+        {
+
+            sRazas =  sRazas + cBufferSocketLong[i];
+
+        }
+
+        if(cBufferSocketLong[i] == '-')
+        {
+
+
+            vRazas.push_back(sRazas);
+            sRazas.clear();
+
+        }
+
+    }
+
+    for(int i = 0; i < vRazas.size() / 2; i++, iCounter =  iCounter + 2)
+    {
+
+        std::cout << vRazas[iCounter] << "    |    " << vRazas[iCounter + 1] << std::endl;
+
+    }
+
+    //Comprobamos las razas disponibles ENVIADAS y COMPROBADAS por el servidor
+    while(!bVerifiedRace)
+    {
+
+        cin >> sInfo;
+
+        for(int i = 0; i < vRazas.size(); i = i+2)
+        {
+
+            if(vRazas[i] == sInfo)
+            {
+                status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
+                bVerifiedRace = true;
+
+            }
+        }
+
+        if(!bVerifiedRace)
+        {
+
+            std::cout << "\n Raza no valida, seleccione una de las listadas." << std::endl;
+
+        }
+
+    }
+
+
+    std::cout << "\nInserte nombre del personaje" << std::endl;
+
+    //Enviamos el nombre del personaje
+    cin >> sInfo;
+    status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
+
+    //Esperamos respuesta del servidor
+    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+
+    //Nombre Ocupado
+    if(cBufferSocket[0] == '1')
+    {
+        //Mientras no ponga un nombre valido.
+        while(!bVerifiedCharacterName)
+        {
+
+            //Enviamos nombre al servidor
+            std::cout << "\nNombre no disponible, inserte otro nombre." << std::endl;
+            cin >> sInfo;
+            status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
+
+            //Esperamos respuesta del servidor
+            status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+
+            //Si el nombre esta libre
+            if(cBufferSocket[0] == '0')
+            {
+
+                bVerifiedCharacterName = true;
+            }
+        }
+    }
+
+    bSelectedCharacter = true;
+    bSelectedListCharacter = true;
+}
 
 int main()
 {
 
 
-    sf::Socket::Status status = socket.connect(SERVER_IP, SERVER_PORT, sf::seconds(15.f));
+    status = socket.connect(SERVER_IP, SERVER_PORT, sf::seconds(15.f));
 
     //El cliente se intentara conectar todo el rato cada 15 segundos hasta conseguirlo.
     if(status != sf::Socket::Done)
@@ -82,11 +193,14 @@ int main()
             //Esperamos respuesta del servidor
             status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
 
+            std::cout << "Recibida password" << std::endl;
             //Contraseña correcta
             if(cBufferSocket[0] == '0')
             {
 
                 bVerified = true;
+                //Esperamos respuesta del servidor
+                status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
 
 
             }//Contraseña incorrecta
@@ -172,216 +286,103 @@ int main()
                 {
                     std::cout << "\nLas contraseñas no coinciden, introduzca de nuevo la contraseña.\n";
                 }
+                CrearPersonaje();
+                bVerified = true;
+
             }
+
+        }
+
+        if(!bSelectedListCharacter)
+        {
+
+
+
+            //Listamos personajes del jugador
 
             system("clear");
 
-            //Seleccionamos raza para el personaje
-            status = socket.receive(cBufferSocketLong, sizeof(cBufferSocketLong), received);
+            std::cout << "Seleccione personaje. Para crear un personaje nuevo escriba Nuevo\n" << std::endl;
 
-
-            std::cout << "Selecciona raza\n" << std::endl;
-
-            for(int i = 0; i < sizeof(cBufferSocketLong); i ++)
+            for(int i = 0; i < sizeof(cBufferSocket); i ++)
             {
 
-                if(cBufferSocketLong[i] != '-')
+                if(cBufferSocket[i] != '-')
                 {
 
-                    sRazas =  sRazas + cBufferSocketLong[i];
+                    sPersonajes =  sPersonajes + cBufferSocket[i];
 
                 }
 
-                if(cBufferSocketLong[i] == '-')
+                if(cBufferSocket[i] == '-')
                 {
 
-
-                    vRazas.push_back(sRazas);
-                    sRazas.clear();
+                    vPersonajes.push_back(sPersonajes);
+                    sPersonajes.clear();
 
                 }
 
             }
 
-            for(int i = 0; i < vRazas.size() / 2; i++, iCounter =  iCounter + 2)
+            for(int i = 0; i < vPersonajes.size(); i++)
             {
 
-                std::cout << vRazas[iCounter] << "    |    " << vRazas[iCounter + 1] << std::endl;
+                std::cout << vPersonajes[i] << std::endl;
 
             }
 
-            //Comprobamos las razas disponibles ENVIADAS y COMPROBADAS por el servidor
-            while(!bVerifiedRace)
+            //Comprobamos los personajes disponibles ENVIADOS y COMPROBADOS por el SERVIDOR
+            while(!bSelectedCharacter)
             {
 
                 cin >> sInfo;
 
-                for(int i = 0; i < vRazas.size(); i = i+2)
+
+                if(sInfo == "Nuevo" || sInfo == "nuevo")
                 {
 
-                    if(vRazas[i] == sInfo)
-                    {
-                        status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-                        bVerifiedRace = true;
-
-                    }
-                }
-
-                if(!bVerifiedRace)
-                {
-
-                    std::cout << "\n Raza no valida, seleccione una de las listadas." << std::endl;
-
-                }
-
-            }
-
-
-            std::cout << "\nInserte nombre del personaje" << std::endl;
-
-
-
-            //Enviamos el nombre del personaje
-            cin >> sInfo;
-            status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-
-            //Esperamos respuesta del servidor
-            status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
-
-            //Nombre Ocupado
-            if(cBufferSocket[0] == '1')
-            {
-                //Mientras no ponga un nombre valido.
-                while(!bVerifiedCharacterName)
-                {
-
-                    //Enviamos nombre al servidor
-                    std::cout << "\nNombre no disponible, inserte otro nombre." << std::endl;
-                    cin >> sInfo;
                     status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
+                    CrearPersonaje();
 
-                    //Esperamos respuesta del servidor
-                    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
 
-                    //Si el nombre esta libre
-                    if(cBufferSocket[0] == '0')
-                    {
 
-                        bVerifiedCharacterName = true;
-                    }
+
+
                 }
-            }
-        }
-
-
-        //Listamos personajes del jugador
-
-        system("clear");
-
-        std::cout << "Seleccione personaje. Para crear un personaje nuevo escriba Nuevo\n" << std::endl;
-
-        for(int i = 0; i < sizeof(cBufferSocket); i ++)
-        {
-
-            if(cBufferSocket[i] != '-')
-            {
-
-                sPersonajes =  sPersonajes + cBufferSocket[i];
-
-            }
-
-            if(cBufferSocket[i] == '-')
-            {
-
-                vPersonajes.push_back(sPersonajes);
-                sPersonajes.clear();
-
-            }
-
-        }
-
-        for(int i = 0; i < vPersonajes.size(); i++)
-        {
-
-            std::cout << vPersonajes[i] << std::endl;
-
-        }
-
-        //Comprobamos los personajes disponibles ENVIADOS y COMPROBADOS por el SERVIDOR
-        while(!bSelectedCharacter)
-        {
-
-            cin >> sInfo;
-
-
-            if(sInfo == "Nuevo" || sInfo == "nuevo")
-            {
-
-            status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-
-
-
-            }
-            else
-            {
-                for(int i = 0; i < vPersonajes.size(); i++)
+                else
                 {
 
-                    if(vPersonajes[i] == sInfo)
+                    for(int i = 0; i < vPersonajes.size(); i++)
                     {
-                        status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-                        bSelectedCharacter = true;
 
+                        if(vPersonajes[i] == sInfo)
+                        {
+                            status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
+                            bSelectedCharacter = true;
+                            bSelectedListCharacter = true;
+
+
+                        }
                     }
                 }
-            }
 
 
-            if(!bSelectedCharacter)
-            {
-
-                std::cout << "\n Personaje no valido, seleccione uno de los listados." << std::endl;
-
-            }
-
-        }
-
-
-
-
-
-
-        //Enviamos el nombre del personaje
-        cin >> sInfo;
-        status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-
-        //Esperamos respuesta del servidor
-        status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
-
-        //Nombre Ocupado
-        if(cBufferSocket[0] == '1')
-        {
-            //Mientras no ponga un nombre valido.
-            while(!bVerifiedCharacterName)
-            {
-
-                //Enviamos nombre al servidor
-                std::cout << "\nNombre no disponible, inserte otro nombre." << std::endl;
-                cin >> sInfo;
-                status = socket.send(sInfo.c_str(), sizeof(sInfo) + 1);
-
-                //Esperamos respuesta del servidor
-                status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
-
-                //Si el nombre esta libre
-                if(cBufferSocket[0] == '0')
+                if(!bSelectedCharacter)
                 {
 
-                    bVerifiedCharacterName = true;
+                    std::cout << "\n Personaje no valido, seleccione uno de los listados." << std::endl;
+
                 }
+
             }
         }
     }
+
+
+
+    std::cout << "ggogo power rangger" << std::endl;
+    cin>> sInfo;
+
 
     return 0;
 }
